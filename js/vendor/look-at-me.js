a8cd9fa4099adb15eab2230f77e1fd3307a443c1
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2018-01-16 18:02:42 +0000

    add filter

--- js/vendor/look-at-me.js
+++ js/vendor/look-at-me.js
@@ -20,10 +20,19 @@
         result.innerHTML = listShow(students);
     };
 
+    var searchEvent = function(e) {
+        e.preventDefault();
+        var students = getStudentsList();
+        var searchName = prompt('Que estudiante(s) deseas buscar');
+        var searchStudents = search(searchName, students);
+        result.innerHTML = listShow(searchStudents);
+    };
+
     
 
     // Manejadores de eventos
     addBoton.addEventListener("click", addEvent);
     showBoton.addEventListener("click", showEvent);
+    searchBoton.addEventListener("click", searchEvent);
     
 }();
